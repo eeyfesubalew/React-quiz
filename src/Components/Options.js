d4d6d3acb9ba18be859878b00404ed61a51a2eea
eeyfesubalew <esubalew.seife@gmail.com>
2023-09-17 16:11:33 +0300

    handling questions

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,8 +1,20 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button key={option} className="btn btn-option">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          disabled={hasAnswer}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
